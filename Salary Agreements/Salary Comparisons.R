9011ba11dd4e8876fb8dac099a7133dcfcd7bc71
ivanxieau <121946904+ivanxieau@users.noreply.github.com>
2023-01-12 05:40:40 +1100

    Public Service Salaries

--- Salary Agreements/Salary Comparisons.R
+++ Salary Agreements/Salary Comparisons.R
@@ -1,11 +1,35 @@
 library(tidyverse)
+library(dplyr)
+library(rvest)
+library(RCurl)
+library(pander)
+library(MASS) 
+library(reshape2) 
+library(reshape) 
 
-# Makes a list of all Salary Comparison files in GitHub.
-df <-
-  data_frame(files = list.files(path = "./yourfilepath",    
-                                pattern = "csv", 
-                                full.names = T)) %>%
-  mutate(dfs = map(files, read_csv, skip = 25)) %>%       
-  unnest() %>% 
-  mutate(cities = str_replace_all(files, "./yourfilepath/", ""),
-         cities = str_replace_all(cities, ".csv", ""))
+# Reads all Salary Comparison CSV files in GitHub.
+
+urlfile ="https://raw.githubusercontent.com/ivanxieau/PublicSector_Australia/main/Salary%20Agreements/ACTPS%20EA%20Salaries.csv" 
+ACT_df <- read_csv(url(urlfile))
+
+urlfile ="https://raw.githubusercontent.com/ivanxieau/PublicSector_Australia/main/Salary%20Agreements/APS%20Remuneration%20Data.csv" 
+Cwth_df <- read_csv(url(urlfile))
+
+urlfile ="https://raw.githubusercontent.com/ivanxieau/PublicSector_Australia/main/Salary%20Agreements/NSWPS%20Award%20Salaries.csv" 
+NSW_df <- read_csv(url(urlfile))
+
+urlfile ="https://raw.githubusercontent.com/ivanxieau/PublicSector_Australia/main/Salary%20Agreements/VPS%20EA%20Salaries.csv" 
+Vic_df <- read_csv(url(urlfile))
+
+urlfile ="https://raw.githubusercontent.com/ivanxieau/PublicSector_Australia/main/Salary%20Agreements/QPS%20CA%20Salaries.csv" 
+Qld_df <- read_csv(url(urlfile))
+
+
+# I need to melt/cast the data since the dates go out -> this way but something isn't working, check code.
+
+
+ACT_df1 <- melt(ACT_df, id.vars =  c("Grade", "Classification", "Jurisdiction")) 
+
+print(ACT_df1) 
+
+
