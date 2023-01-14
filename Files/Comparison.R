3448678696b2ec5ec796a73be4ad7acc4fe01957
ivanxieau <121946904+ivanxieau@users.noreply.github.com>
2023-01-15 07:05:26 +1100

    Cleaning data

--- Files/Comparison.R
+++ Files/Comparison.R
@@ -8,29 +8,29 @@ library(MASS)
 library(reshape)
 library(reshape2)
 
-# Read all Salary Comparison CSV files in GitHub.
-
-urlfile ="XXXX" 
+# Read all comparison files from GitHub.
+urlfile ="https://raw.githubusercontent.com/ivanxieau/Australian_Public_Sector/main/Files/Salaries.csv" 
 Salaries <- read_csv(url(urlfile))
-
 urlfile ="https://raw.githubusercontent.com/ivanxieau/Australian_Public_Sector/main/Files/ABS_Pay_Data.csv"
 Occp <- read_csv(url(urlfile))
-
 urlfile ="https://raw.githubusercontent.com/ivanxieau/Australian_Public_Sector/main/Files/Daily_CPI_Inflator.csv" 
 CPI <- read_csv(url(urlfile))
 
 # Melt ABS dataframe
 Occp <- melt(Occp, id = c("Code", "Occupation", "Date"))
+Occp <- na.omit(Occp)
+
+write.csv(Occp, "C:\\Users\\ivanx\\Desktop\\O.csv", row.names=FALSE)
 
 # Clean CPI dataframe
 CPI$Date <- as.Date(CPI$Date, "%d/%m/%Y")
 
 # Combining PS Salaries and CPI dataframes
-PS_Salaries <- left_join(PS_Salaries, CPI, by = "Date")
-PS_Salaries <- subset(PS_Salaries, select = -c(Grade, SAdj_CPI_Index, Daily_CPI))
-PS_Salaries$Inflator <- as.numeric(gsub("%","",PS_Salaries$Inflator))
-PS_Salaries$Inflator <- as.numeric(PS_Salaries$Inflator/100)
-PS_Salaries <- PS_Salaries %>% mutate(Salary_Real = Salary * (1 + Inflator/100))
+Salaries <- left_join(Salaries, CPI, by = "Date")
+Salaries <- subset(Salaries, select = -c(Grade, SAdj_CPI_Index, Daily_CPI))
+Salaries$Inflator <- as.numeric(gsub("%","",Salaries$Inflator))
+Salaries$Inflator <- as.numeric(Salaries$Inflator/100)
+Salaries <- Salaries %>% mutate(Salary_Real = Salary * (1 + Inflator/100))
 
 # Combining Occupation and CPI dataframes, convert weekly to annual salaries
 Occp <- left_join(Occp, CPI, by = "Date")
@@ -51,7 +51,7 @@ PS_Summ <- PS_Salaries %>%
 
 # Clean and print final dataframes
 PS_Summ <- na.omit(PS_Summ)
-Oc_Summ <- na.omit(Occp)
+
 
 print(PS_Summ)
 print(Oc_Summ)
